93153e22e0d8b1bc50c8532c8b6b4fb87f958d6b
Yumini <yuminc03@gmail.com>
2023-04-21 22:43:33 +0900

    Initial Commit

---
+++ LabsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabsProject
+//
+//  Created by Yumin Chu on 2023/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
