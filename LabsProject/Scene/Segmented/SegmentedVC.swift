770bf1e29757dbcb33384fcecfd9bcdf67bad787
Yumini <yuminc03@gmail.com>
2023-06-22 22:42:36 +0900

    Feat: Network 통신을 위한 파일 준비, SegmentedVC 생성

---
+++ LabsProject/Scene/Segmented/SegmentedVC.swift
@@ -0,0 +1,28 @@
+//
+//  SegmentedVC.swift
+//  LabsProject
+//
+//  Created by Yumin Chu on 2023/06/22.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SegmentedVC: LabsVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstarints()
+        
+    }
+    
+    private func setupUI() {
+        
+    }
+    
+    private func setupConstarints() {
+        
+    }
+}
