f72b1e85be380f96a060d6bbc54e201958e8daec
Yumini <yuminc03@gmail.com>
2023-07-20 01:20:19 +0900

    Feat: Animation Reducer 추가

---
+++ LabsProject/Scene/TCA/Reducer/Animations.swift
@@ -0,0 +1,16 @@
+//
+//  Animations.swift
+//  LabsProject
+//
+//  Created by Yumin Chu on 2023/07/19.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+struct Animations: ReducerProtocol {
+    struct State: Equatable {
+        
+    }
+}
