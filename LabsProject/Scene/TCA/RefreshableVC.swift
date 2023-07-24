d38822578a8f8464379b2fbfbc68d4853f864fdc
Yumini <yuminc03@gmail.com>
2023-07-24 21:58:21 +0900

    Refactor: CounterView를 따로 만들기, RefreshableVC 생성

---
+++ LabsProject/Scene/TCA/RefreshableVC.swift
@@ -0,0 +1,21 @@
+//
+//  RefreshableVC.swift
+//  LabsProject
+//
+//  Created by Yumin Chu on 2023/07/24.
+//
+
+import UIKit
+import Combine
+
+import CombineCocoa
+import ComposableArchitecture
+import SnapKit
+
+struct Refreshable: ReducerProtocol {
+    
+}
+
+final class RefreshableVC: TCABaseVC<Refreshable> {
+    
+}
