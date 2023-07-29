adf68abf81946fc49372f7a954bde47d245b237a
Yumini <yuminc03@gmail.com>
2023-07-30 00:09:37 +0900

    Feat: Reducer 파일 분리, Reduce 내부 action 구현

---
+++ LabsProject/Scene/TCA/Reducer/EagerNavigation.swift
@@ -0,0 +1,57 @@
+//
+//  EagerNavigation.swift
+//  LabsProject
+//
+//  Created by Yumin Chu on 2023/07/30.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+struct EagerNavigation: ReducerProtocol {
+    struct State: Equatable {
+        var isNavigationActive = false
+        var optionalCounter: Counter.State?
+    }
+    
+    enum Action {
+        case optionalCounter(Counter.Action)
+        case setNavigation(isActive: Bool)
+        case setNavigationIsActiveDelayCompleted
+    }
+    
+    private enum CancelID {
+        case load
+    }
+    @Dependency(\.continuousClock) var clock
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .setNavigation(isActive: true):
+                state.isNavigationActive = true
+                return .run { send in
+                    try await self.clock.sleep(for: .seconds(1))
+                    await send(.setNavigationIsActiveDelayCompleted)
+                }
+                .cancellable(id: CancelID.load)
+                
+            case .setNavigation(isActive: false):
+                state.isNavigationActive = false
+                state.optionalCounter = nil
+                return .cancel(id: CancelID.load)
+                
+            case .setNavigationIsActiveDelayCompleted:
+                state.optionalCounter = Counter.State()
+                return .none
+                
+            case .optionalCounter:
+                return .none
+            }
+        }
+        .ifLet(\.optionalCounter, action: /Action.optionalCounter) {
+            Counter()
+        }
+    }
+}
