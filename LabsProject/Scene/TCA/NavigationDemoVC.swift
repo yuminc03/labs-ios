4b358b34642a760706432f1476552c6c5d112df5
Yumini <yuminc03@gmail.com>
2023-08-04 23:50:32 +0900

    Feat: ScreenA reduce() 메서드 구현

--- LabsProject/Scene/TCA/NavigationDemoVC.swift
+++ LabsProject/Scene/TCA/NavigationDemoVC.swift
@@ -59,7 +59,40 @@ struct ScreenA: ReducerProtocol {
     @Dependency(\.dismiss) var dismiss
     @Dependency(\.factClient) var factClient
     
-    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .didTapDecrementButton:
+            state.count -= 1
+            return .none
+            
+        case .didTapDismissButton:
+            return .fireAndForget {
+                await dismiss()
+            }
+            
+        case .didTapIncrementButton:
+            state.count += 1
+            return .none
+            
+        case .didTapFactButton:
+            state.isLoading = true
+            return .task { [count = state.count] in
+                await .factResponse(.init { try await
+                    factClient.fetch(count)
+                })
+            }
+            
+        case let .factResponse(.success(fact)):
+            state.isLoading = false
+            state.fact = fact
+            return .none
+            
+        case .factResponse(.failure):
+            state.isLoading = false
+            state.fact = nil
+            return .none
+        }
+    }
 }
 final class NavigationDemoVC: TCABaseVC<NavigationDemo> {
     
