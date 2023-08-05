e3b7b24091f0174dd64068eeb950b57c5d0ee3fe
Yumini <yuminc03@gmail.com>
2023-08-05 23:09:40 +0900

    Feat: 구현 덜한 Reducer 구현 완료

--- LabsProject/Scene/TCA/NavigationDemoVC.swift
+++ LabsProject/Scene/TCA/NavigationDemoVC.swift
@@ -13,7 +13,6 @@ import ComposableArchitecture
 import SnapKit
 
 struct NavigationDemo: ReducerProtocol {
-    
     struct State: Equatable {
         var path = StackState<Path.State>()
     }
@@ -31,9 +30,37 @@ struct NavigationDemo: ReducerProtocol {
             case let .goBackToScreen(id):
                 state.path.pop(to: id)
                 return .none
+                
+            case .didTapGoToABCButton:
+                state.path.append(.screenA())
+                state.path.append(.screenB())
+                state.path.append(.screenC())
+                return .none
+                
+            case let .path(action):
+                switch action {
+                case .element(id: _, action: .screenB(.didTapScreenAButton)):
+                    state.path.append(.screenA())
+                    return .none
+                    
+                case .element(id: _, action: .screenB(.didTapScreenBButton)):
+                    state.path.append(.screenB())
+                    return .none
+                    
+                case .element(id: _, action: .screenB(.didTapScreenCButton)):
+                    state.path.append(.screenC())
+                    return .none
+                    
+                default:
+                    return .none
+                }
+                
+            case .popToRoot:
+                state.path.removeAll()
+                return .none
             }
         }
-        .ifLet(\.path, action: /Action.path) {
+        .forEach(\.path, action: /Action.path) {
             Path()
         }
     }
@@ -41,8 +68,26 @@ struct NavigationDemo: ReducerProtocol {
     struct Path: ReducerProtocol {
         enum State: Codable, Equatable, Hashable {
             case screenA(ScreenA.State = .init())
-//            case screenB(ScreenB.State = .init())
-//            case screenC(ScreenC.State = .init())
+            case screenB(ScreenB.State = .init())
+            case screenC(ScreenC.State = .init())
+        }
+        
+        enum Action {
+            case screenA(ScreenA.Action)
+            case screenB(ScreenB.Action)
+            case screenC(ScreenC.Action)
+        }
+        
+        var body: some ReducerProtocol<State, Action> {
+            Scope(state: /State.screenA, action: /Action.screenA) {
+                ScreenA()
+            }
+            Scope(state: /State.screenB, action: /Action.screenB) {
+                ScreenB()
+            }
+            Scope(state: /State.screenC, action: /Action.screenC) {
+                ScreenC()
+            }
         }
     }
 }
@@ -101,13 +146,77 @@ struct ScreenA: ReducerProtocol {
     }
 }
 
+struct ScreenB: ReducerProtocol {
+    struct State: Codable, Equatable, Hashable {
+        
+    }
+    
+    enum Action {
+        case didTapScreenAButton
+        case didTapScreenBButton
+        case didTapScreenCButton
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .didTapScreenAButton:
+            return .none
+            
+        case .didTapScreenBButton:
+            return .none
+            
+        case .didTapScreenCButton:
+            return .none
+        }
+    }
+}
+
+struct ScreenC: ReducerProtocol {
+    struct State: Codable, Equatable, Hashable {
+        var count = 0
+        var isTimerRunning = false
+    }
+    
+    enum Action {
+        case didTapStartButton
+        case didTapStopButton
+        case timerTick
+    }
+    
+    @Dependency(\.mainQueue) var mainQueue
+    enum CancelID {
+        case timer
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .didTapStartButton:
+            state.isTimerRunning = true
+            return .run { send in
+                for await _ in mainQueue.timer(interval: 1) {
+                    await send(.timerTick)
+                }
+            }
+            .cancellable(id: CancelID.timer)
+            .concatenate(with: .init(value: .didTapStopButton))
+            
+        case .didTapStopButton:
+            state.isTimerRunning = false
+            return .cancel(id: CancelID.timer)
+            
+        case .timerTick:
+            state.count += 1
+            return .none
+        }
+    }
+}
+
 final class NavigationDemoVC: TCABaseVC<NavigationDemo> {
     
     init() {
-        store = Store(
+        let store = Store(
             initialState: NavigationDemo.State(),
             reducer: NavigationDemo())
         super.init(store: store)
     }
 }
-
