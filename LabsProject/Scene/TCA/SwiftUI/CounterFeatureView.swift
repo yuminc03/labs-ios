da451bf77480677401a02c0814cc933154c5da4d
추유민 <ym.chu@lifesemantics.kr>
2023-08-11 15:25:15 +0900

    Refactor: viewStore 밖에서 만들고 사용

--- LabsProject/Scene/TCA/SwiftUI/CounterFeatureView.swift
+++ LabsProject/Scene/TCA/SwiftUI/CounterFeatureView.swift
@@ -11,34 +11,49 @@ import ComposableArchitecture
 
 struct CounterFeatureView: View {
     let store: StoreOf<CounterFeature>
+    @ObservedObject var viewStore: ViewStoreOf<CounterFeature>
+    
+    init(store: StoreOf<CounterFeature>) {
+        self.store = store
+        self.viewStore = ViewStore(store, observe: { $0 })
+    }
     
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            VStack {
-                Text("\(viewStore.count)")
-                    .font(.largeTitle)
-                    .padding()
-                    .background(Color.black.opacity(0.1))
-                    .cornerRadius(10)
-                HStack {
-                    Button("-") {
-                        store.send(.didTapDecrementButton)
-                    }
-                    .font(.largeTitle)
-                    .padding()
-                    .background(Color.black.opacity(0.1))
-                    .cornerRadius(10)
-                    
-                    Button("+") {
-                        store.send(.didTapIncrementButton)
-                    }
-                    .font(.largeTitle)
-                    .padding()
-                    .background(Color.black.opacity(0.1))
-                    .cornerRadius(10)
+        VStack {
+            Text("\(viewStore.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.didTapDecrementButton)
                 }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("+") {
+                    store.send(.didTapIncrementButton)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
             }
         }
+    }
+}
+
+extension View {
+    
+    func common() -> some View {
+        self
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
     }
 }
 
